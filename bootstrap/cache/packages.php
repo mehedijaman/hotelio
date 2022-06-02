ff0b4aa4c1161eab3db2568bb747b95b63b2da1f
Enamulhqenam <enamulhqenam50607210@gmail.com>
2022-06-02 21:45:59 +0600

    create a new folder on view/Documents/Er_Diagram

--- bootstrap/cache/packages.php
+++ bootstrap/cache/packages.php
@@ -1,4 +1,11 @@
 <?php return array (
+  'laravel/breeze' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Breeze\\BreezeServiceProvider',
+    ),
+  ),
   'laravel/sail' => 
   array (
     'providers' => 
