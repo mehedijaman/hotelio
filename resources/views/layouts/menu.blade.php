4a979658b2c6af5a572acc97e584f1e03d0ebc1f
Enamulhqenam <enamulhqenam50607210@gmail.com>
2022-06-14 20:41:06 +0600

    fix

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -97,6 +97,14 @@
     </div>
 </div>
 
+
+
+
+
+
+
+
+
 <li class="nav-item">
     <a href="/null" class="nav-link {{ Request::is('null') ? 'active' : '' }}">
         <i class="nav-icon fas fa-briefcase"></i>
