6bf7a72439f450514058b2cda7d84431393ba83f
Enamulhqenam <enamulhqenam50607210@gmail.com>
2022-06-22 16:21:51 +0600

    fix expense table edit and delete button design

--- resources/views/expense/index.blade.php
+++ resources/views/expense/index.blade.php
@@ -30,29 +30,21 @@
                                         <td>{{ $Expense->Amount }}</td>
                                         <td>{{ $Expense->Description }}</td>
                                         <td>{{ $Expense->Date }}</td>
-                                        <td class="">
-                                            {{-- <svg data-v-9a6e255c="" xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="invoice-row-5036-send-icon" class="cursor-pointer feather feather-send"><line data-v-9a6e255c="" x1="22" y1="2" x2="11" y2="13"></line><polygon data-v-9a6e255c="" points="22 2 15 22 11 13 2 9 22 2"></polygon></svg> --}}
-
-                                            <svg data-v-9a6e255c="" xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="invoice-row-5036-preview-icon" class="mx-1 feather feather-eye"><path data-v-9a6e255c="" d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle data-v-9a6e255c="" cx="12" cy="12" r="3"></circle>
-                                            </svg>
-
-                                            <span class="dropdown">
-                                                <button class="" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false"><svg data-v-9a6e255c="" xmlns="http://www.w3.org/2000/svg" width="16px" height="16px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="align-middle text-body feather feather-more-vertical"><circle data-v-9a6e255c="" cx="12" cy="12" r="1"></circle><circle data-v-9a6e255c="" cx="12" cy="5" r="1"></circle><circle data-v-9a6e255c="" cx="12" cy="19" r="1"></circle>
-                                                </svg></button>
-                                                <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                                                    <li>
-                                                        <a class="dropdown-item" href="/expense/{{ $Expense->id }}/edit" style="letter-spacing:3px; font-family:'Courier New', Courier, monospace ;">
-                                                            <i class="fa-regular fa-pen-to-square mr-2"></i>Edit
-                                                        </a>
-                                                    </li>
-                                                    {!! Form::open(['url' => '/expense/'.$Expense->id, 'method' => 'DELETE']) !!}
-                                                        <i class="fa-regular fa-trash-can mr-2" style="margin-left:10%;">
-                                                            <input type="submit" name="submit" value=" Delete" style="letter-spacing:3px; font-family:'Courier New', Courier, monospace ;" class="bg-danger pt-md-3 pb-md-3">
-                                                        </i>
-                                                    {!! Form::close() !!}
-                                                </ul>
-                                            </span>
-                                        </td>
+                                        <td class="d-flex">
+                                            <a href="" class="mr-3 text-purple" data-bs-toggle="tooltip" data-bs-placement="bottom" title="View">
+                                                 <svg data-v-9a6e255c="" xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="invoice-row-5036-preview-icon" class="mx-1 feather feather-eye"><path data-v-9a6e255c="" d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle data-v-9a6e255c="" cx="12" cy="12" r="3"></circle></svg>
+                                            </a>
+                                             <a class="" href="/expense/{{ $Expense->id }}/edit" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Edit">
+                                                 <i class="fa-regular fa-pen-to-square mr-3 text-orange"></i></i>
+                                             </a>
+                                            
+                                             {{ Form::open(array('url' => '/expense/'.$Expense->id,'method' => 'DELETE')) }}
+                                                 <button class="" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Delete">
+                                                     <i class="fa-regular fa-trash-can mr-3 text-danger"></i>
+                                                 </button>
+                                             {{ Form::close() }}
+                                                    
+                                         </td>
                                     </tr>
                                 @endforeach
                             </tbody>
