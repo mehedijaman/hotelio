85c59bda8614ed8c338860f988cf99b3864dc6ef
M.Fedawus sk <ferdawusa@gmail.com>
2022-06-29 10:50:18 +0600

    invoice category Desing

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -1,4 +1,77 @@
 @extends('layouts.app')
 @section('content')
-    <a href="/invoice/create" class="btn btn-primary">create</a>
+   
+    <div class="container py-5">
+        {{-- <section class="button mb-4">
+            <a href="{{ asset('booking/create') }}" class="btn btn-info text-capitalize"> <i class="fa-solid fa-circle-plus mr-2"></i>Add</a>
+        </section> --}}
+        <div class="row">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header bg-defult">
+                        <div class="card-title">
+                            <h2 class="card-title">
+                                <a href="{{ asset('invoice/create') }}" class="btn bg-navy text-capitalize mr-3" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Create Booking"> 
+                                    <i class="fa-solid fa-circle-plus mr-2"></i>
+                                    Add
+                                </a>
+                                Booking List
+                            </h2>
+                        </div>
+                        <a class="btn btn-sm bg-navy float-right text-capitalize" href="/invoice/trash"><i class="fa-solid fa-recycle mr-2"></i>View Trash</a>
+                        <a class="btn btn-sm bg-maroon float-right text-capitalize mr-3" href="/invoice/delete"><i class="fa-solid fa-trash-can mr-2"></i>Delete All</a>
+                    </div>
+                    <div class="card-body table-responsive p-0">
+                        <table class="table table-hover text-nowrap">
+                            <thead>
+                                <tr>
+                                    <th>Guest</th>
+                                    <th>Tax</th>
+                                    <th>PaymentMethod</th>
+                                    <th>Date</th>
+                                    <th>SubTotal</th>
+                                    <th>TaxTotal</th>
+                                    <th>Total</th>
+                                    <th>Action</th>
+                                </tr>
+                            </thead>
+
+                            <tbody>
+                                {{-- @foreach ($Bookings as $Booking)
+                                    <tr class="">
+                                        <td>{{$Booking->RoomNo}}</td>
+                                        <td>{{$Booking->Guest}}</td>
+                                        <td>
+                                            @php
+                                                echo date('d/m/Y H:i:s',strtotime($Booking->CheckInDate))
+                                            @endphp
+                                        </td>
+                                        <td>{{$Booking->CheckOutDate}}</td>
+                                        <td class="d-flex">
+                                           <a href="{{ URL::to('/invoice/'.$Booking->id) }}" class="mr-3 text-purple" data-bs-toggle="tooltip" data-bs-placement="bottom" title="View">
+                                            <svg data-v-9a6e255c="" xmlns="http://www.w3.org/2000/svg" width="18px" height="18px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" id="invoice-row-5036-preview-icon" class="mx-1 feather feather-eye"><path data-v-9a6e255c="" d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle data-v-9a6e255c="" cx="12" cy="12" r="3"></circle></svg>
+                                           </a>
+                                            <a class="" href="/invoice/{{ $Booking->id }}/edit" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Edit">
+                                                <i class="fa-regular fa-pen-to-square mr-3 text-orange"></i></i>
+                                            </a>
+                                            
+                                            {{ Form::open(array('url' => '/invoice/'.$Booking->id,'method' => 'DELETE')) }}
+                                                <button class="" data-bs-toggle="tooltip" data-bs-placement="bottom" title="Delete">
+                                                    <i class="fa-regular fa-trash-can mr-3 text-danger"></i>
+                                                </button>
+                                            {{ Form::close() }} 
+                                        </td>
+                                    </tr>
+                                @endforeach --}}
+                            </tbody>
+                           
+                        </table>
+                    </div>
+                    <div class="card-footer">
+                     
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
