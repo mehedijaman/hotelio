97f33e39ce8d819c1b69b07b6c88978f27a68add
M.Fedawus sk <ferdawusa@gmail.com>
2022-07-05 16:50:52 +0600

    profle page modified

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -6,19 +6,41 @@
                 <div class="card card-primary card-outline">
                     <div class="card-body">
                         <div class="text-center">
-                            {{-- <img src="{{URL::asset('/img/profile.png')}}" alt="User_img" class="profile-user-img img-fluid img-circle"> --}}
                             @if (Auth::user()->Photo)
                                 <img src="/uploads/{{Auth::user()->Photo}}" alt="User_img" class="profile-user-img img-fluid img-circle">
                                 @else
-                                     <img src="{{URL::asset('/img/profile.png')}}" alt="User_img" class="profile-user-img img-fluid img-circle">
+                                    <img src="{{URL::asset('/img/profile.png')}}" alt="User_img" class="profile-user-img img-fluid img-circle">
                             @endif
                         </div>
-                        {{-- {{ Auth::user()->name }}
-                            <small>Member since {{ Auth::user()->created_at->format('D-M. Y') }}</small>
-                        {{ Auth::user()->email }}
-                        {{ Auth::user()->Status }}
-                        {{ Auth::user()->Role }} --}}
-                            
+                        <h3 class="profile-username text-center">{{ Auth::user()->name }}</h3>
+                        <p class="text-muted text-center">
+                            Member since 
+                            {{ Auth::user()->created_at->format('D.M. Y') }}
+                        </p>
+                        <ul class="list-group list-group-unbordered mb-3">
+                             <li class="list-group-item">
+                                <b>Role</b>
+                                <a href="#" class="text-dark float-right">{{ Auth::user()->Role }}</a>
+                            </li>
+                             <li class="list-group-item">
+                                <b>Status</b>
+                                <a href="#" class="text-dark float-right"> 
+                                    @if(Auth::user()->Status) <b class="text-success">Active</b>
+                                    @endif
+                                    @if(!Auth::user()->Status) <b class="text-danger">Active</b>
+                                    @endif
+                                </a>
+                            </li>
+                            <li class="list-group-item">
+                                <b>Email</b>
+                                <a href="#" class=" text-dark float-right">{{ Auth::user()->email }}</a>
+                            </li>
+                            <li class="list-group-item">
+                                <b>LastLogin</b>
+                                <a href="#" class=" text-dark float-right">{{ Auth::user()->LastLogin }}</a>
+                            </li>
+                        </ul>
+                        <a href="" class="btn btn-primary btn-block text-capitalize">Edit Profile</a>
                     </div>
                 </div>
             </div>
