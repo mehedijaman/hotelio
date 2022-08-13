9ce2eead0df1bfe4ab58f7a7680cc87a7c8886ca
Mehedi Jaman <mail4mjaman@gmail.com>
2022-08-13 10:07:00 +0600

    - API Introduction

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\BankController as BankControllerV1;
+use App\Http\Controllers\Api\V2\BankController as BankControllerV2;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +16,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['prefix' => 'v1'], function(){
+    Route::resource('/bank', BankControllerV1::class);
+});
+
+Route::group(['prefix' => 'v2'], function(){
+    Route::resource('/bank', BankController::class)->middleware('auth:sanctum');
 });
